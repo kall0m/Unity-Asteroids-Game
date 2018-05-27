68aad287b4ad63e2676a128461627bcc243396f8
Kaloyan Madjunov <k.madjunov@gmail.com>
2018-05-27 22:41:27 +0300

    Add Menu scene and Start and Exit buttons

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour {
+    public void StartGameBtn(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    public void ExitGameBtn()
+    {
+        Application.Quit();
+    }
+}
